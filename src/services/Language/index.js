ea8d1726c793e9862fa87f6673687cdc3c78c67d
bwd <bloomerwd@gmail.com>
2017-10-10 17:44:53 +0600

    Added locales for English and Russian languages

---
+++ src/services/Language/index.js
@@ -0,0 +1,2 @@
+import Language from './Language';
+export default Language;
---
+++ src/services/Language/strings.js
@@ -0,0 +1,12 @@
+const Strings = {
+  // English language
+  en: {
+    helpText: 'Material Colors — tool for creating colors palette your Android-application and testing them on principal of Android Material Design. You can see how will look main elements of interface and how they connected with each other.'
+  },
+  // Russian language
+  ru: {
+    helpText: 'Material Colors — инструмент для создания цветовых схем вашего Android-приложения и их проверки на соответствие принципам Android Material Design. С ним вы увидите, как будут выглядеть основные элементы интерфейса и как они связаны друг с другом.'
+  }
+}
+
+export default Strings;
