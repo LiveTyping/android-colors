0e78633fa929c116400bfbd59f4d33e44028334f
bwd <bloomerwd@gmail.com>
2017-10-12 16:55:38 +0600

    fix/refactoring-language-function

--- src/services/Language/Language.js
+++ src/services/Language/Language.js
@@ -3,11 +3,12 @@ import Strings from './strings';
 // lang arg take a user language - RU / EN and return a text in current language
 const Language = () => {
   const userLang = window.navigator.language;
-  // if user local language is English - return text on English language
-  if (userLang.match(/^en/g)) return Strings.en;
   // if user local language is Russian - return text on Russian language
-  else if (userLang.match(/^ru/g)) return Strings.ru;
-  return false;
+  if (userLang === 'ru') {
+    return Strings.ru;
+  }
+  // else if user language is not Russian - return text on English language
+  return Strings.en;
 };
 
 export default Language;
